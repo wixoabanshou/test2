094394fddbf4b2b1a7b48425ccc328acd4e6af59
ChenShen <2556837865@qq.com>
2020-02-13 11:10:38 +0800

    提交到本地

--- Day02/src/main/java/Demo1.java
+++ Day02/src/main/java/Demo1.java
@@ -1,6 +1,16 @@
 public class Demo1 {
 
+
+
     public static void main(String[] args) {
         System.out.println("diyicitijiao");
+    }
+
+
+
+    public void test(){
+        int i= 0;
+        i+=5;
+        System.out.println(i);
     }
 }
